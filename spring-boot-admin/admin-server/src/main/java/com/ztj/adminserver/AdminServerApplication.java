9421f501fc273403e374024256729a2d131cafe9
tj.zhao <tj.zhao@hnair.com>
2018-11-16 15:41:43 +0800

    spring boot admin demo

---
+++ spring-boot-admin/admin-server/src/main/java/com/ztj/adminserver/AdminServerApplication.java
@@ -0,0 +1,59 @@
+package com.ztj.adminserver;
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer // 开启监控功能
+@EnableEurekaClient // 把 Spring Boot Admin注册到Eureka里，就可以发现注册到Eureka里的其他服务实例
+public class AdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminServerApplication.class, args);
+	}
+
+	@Profile("insecure")
+	@Configuration
+	public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests().anyRequest().permitAll().and().csrf().disable();
+		}
+	}
+
+	@Profile("secure")
+	@Configuration
+	public static class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
+		private final String adminContextPath;
+
+		public SecuritySecureConfig(AdminServerProperties adminServerProperties) {
+			this.adminContextPath = adminServerProperties.getContextPath();
+		}
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			// @formatter:off
+			SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+			successHandler.setTargetUrlParameter("redirectTo");
+
+			http.authorizeRequests()
+					.antMatchers(adminContextPath + "/login").permitAll()
+					.anyRequest().authenticated()
+					.and()
+					.formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()
+					.logout().logoutUrl(adminContextPath + "/logout").and()
+					.httpBasic().and()
+					.csrf().disable();
+			// @formatter:on
+		}
+	}
+}
---
+++ spring-boot-admin/service-demo/src/test/java/com/ztj/servicedemo/ServiceDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ztj.servicedemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServiceDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
